6f14efe6ddb80db435568581fe268c16cf6be0b6
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-03 10:21:24 +0900

    004-custom-layout-setting

--- nuxt-portfolio/pages/about.vue
+++ nuxt-portfolio/pages/about.vue
@@ -1,3 +1,15 @@
 <template>
   <div>This page is about me.</div>
+  <!-- <div>
+    <button @click="enableCustomLayout">Update Layout</button>
+  </div> -->
 </template>
+
+<script setup>
+// definePageMeta({
+//   layout: "another",
+// });
+// const enableCustomLayout = () => {
+//   setPageLayout("another");
+// };
+</script>
