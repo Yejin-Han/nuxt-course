f063034d7033766c5296e86119514226a0bbd559
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-03 17:50:03 +0900

    014-markdown-typography

--- nuxt-portfolio/pages/blog/[...slug].vue
+++ nuxt-portfolio/pages/blog/[...slug].vue
@@ -1,5 +1,6 @@
 <template>
-  <article>
+  <article class="prose dark:prose-invert">
+    <!-- tailwind 적용시 모든 스타일이 무효화되는 점으로 인해 markdown 서식도 무효화되는 경우, @tailwindcss/typography를 이용하여 해당 부분만 css reset을 하지 않음 -->
     <ContentDoc />
   </article>
 </template>
