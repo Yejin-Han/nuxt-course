59fca3e1dd08134bbced97e0a32d227e1cc36201
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-06 12:44:54 +0900

    018-post-list-querying

--- nuxt-portfolio/pages/blog/index.vue
+++ nuxt-portfolio/pages/blog/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>All blog posts list!</div>
+  <section>
+    <ul>
+      <li v-for="(post, idx) in posts" :key="idx">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup>
+const { data: posts } = await useAsyncData("blog-list", () =>
+  queryContent("/blog").only(["_path", "title"]).find()
+);
+console.log(posts);
+</script>
