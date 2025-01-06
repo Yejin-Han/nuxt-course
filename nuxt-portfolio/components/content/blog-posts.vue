6986bd526cc15ef0291826c90470da5a7a8cfc9f
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-06 13:36:59 +0900

    020-blog-posts-redesign

--- nuxt-portfolio/components/content/blog-posts.vue
+++ nuxt-portfolio/components/content/blog-posts.vue
@@ -1,8 +1,18 @@
 <template>
-  <section class="not-prose">
+  <section class="not-prose font-mono">
+    <div class="column text-gray-400 text-sm">
+      <div>date</div>
+      <div>title</div>
+    </div>
     <ul>
       <li v-for="(post, idx) in posts" :key="idx">
-        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+        <NuxtLink
+          :to="post._path"
+          class="column hover:bg-gray-100 dark:hover:bg-gray-800"
+        >
+          <div class="text-gray-500">2023</div>
+          <div>{{ post.title }}</div>
+        </NuxtLink>
       </li>
     </ul>
   </section>
@@ -16,3 +26,9 @@ const { data: posts } = await useAsyncData("blog-list", () =>
     .find()
 );
 </script>
+
+<style scoped>
+.column {
+  @apply flex items-center space-x-8 py-2 border-b border-gray-200 dark:border-gray-700;
+}
+</style>
