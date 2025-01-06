f76bcda1a21d647bbd0d42672f3d755e9b59cdd7
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-06 13:24:44 +0900

    019-mdc-syntax

--- nuxt-portfolio/components/content/blog-posts.vue
+++ nuxt-portfolio/components/content/blog-posts.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="not-prose">
     <ul>
       <li v-for="(post, idx) in posts" :key="idx">
         <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
@@ -10,7 +10,9 @@
 
 <script setup>
 const { data: posts } = await useAsyncData("blog-list", () =>
-  queryContent("/blog").only(["_path", "title"]).find()
+  queryContent("/blog")
+    .where({ _path: { $ne: "/blog" } }) // 목록을 보여줄 /blog path가 목록에 포함되어 뜨기 때문에 제외시켜줌
+    .only(["_path", "title"])
+    .find()
 );
-console.log(posts);
 </script>
