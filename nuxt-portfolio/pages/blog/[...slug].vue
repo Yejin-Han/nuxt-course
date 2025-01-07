96a7c3380532282ff7ac4b3cfaea0bab9e088cae
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-07 16:17:49 +0900

    027-error-pages

--- nuxt-portfolio/pages/blog/[...slug].vue
+++ nuxt-portfolio/pages/blog/[...slug].vue
@@ -3,20 +3,26 @@
     class="prose dark:prose-invert max-w-none prose-pre:bg-gray-100 dark:prose-pre:bg-gray-800 prose-pre:text-gray-600 dark:prose-pre:text-gray-200"
   >
     <!-- tailwind 적용시 모든 스타일이 무효화되는 점으로 인해 markdown 서식도 무효화되는 경우, @tailwindcss/typography를 이용하여 해당 부분만 css reset을 하지 않음 -->
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
-          <ContentRenderer :value="doc" />
+    <ContentDoc>
+      <template #not-found>
+        <h1>Document not found (404)</h1>
+        <p>This blog post could not be found.</p>
+      </template>
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+            <ContentRenderer :value="doc" />
+          </div>
+          <div class="col-span-2 not-prose" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">Table of Contents</div>
+              <nav>
+                <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+              </nav>
+            </aside>
+          </div>
         </div>
-        <div class="col-span-2 not-prose" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">Table of Contents</div>
-            <nav>
-              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
-            </nav>
-          </aside>
-        </div>
-      </div>
+      </template>
     </ContentDoc>
   </article>
 </template>
