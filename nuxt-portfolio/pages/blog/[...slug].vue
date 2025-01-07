23eb7ea557b6490e56c880546790eac7d77eb8aa
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-07 15:32:17 +0900

    026-toc-active-section

--- nuxt-portfolio/pages/blog/[...slug].vue
+++ nuxt-portfolio/pages/blog/[...slug].vue
@@ -4,7 +4,6 @@
   >
     <!-- tailwind 적용시 모든 스타일이 무효화되는 점으로 인해 markdown 서식도 무효화되는 경우, @tailwindcss/typography를 이용하여 해당 부분만 css reset을 하지 않음 -->
     <ContentDoc v-slot="{ doc }">
-      {{ doc.body.toc.links }}
       <div class="grid grid-cols-6 gap-16">
         <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
           <ContentRenderer :value="doc" />
@@ -13,7 +12,7 @@
           <aside class="sticky top-8">
             <div class="font-semibold mb-2">Table of Contents</div>
             <nav>
-              <TocLinks :links="doc.body.toc.links" />
+              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
             </nav>
           </aside>
         </div>
@@ -23,6 +22,35 @@
 </template>
 
 <script setup>
-const route = useRoute();
-console.log(route.params.slug);
+const activeId = ref(null);
+const observer = ref(null);
+
+// 여기서 onMounted를 쓰는 이유는, headers(h2, h3 등)가 Dom에 먼저 들어가 있어야 하기 때문
+// client side에서만 실행되므로, 브라우저 전역 객체(window, document 등)를 사용할 수 있게 된다. -> Browser API인 intersectionObserver도 사용 가능한 것.
+onMounted(() => {
+  const callback = (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        activeId.value = entry.target.id;
+        console.log(entry.target.id, activeId.value);
+        break;
+      }
+    }
+  };
+  observer.value = new IntersectionObserver(callback, {
+    root: null, // =document
+    threshold: 0.5,
+  });
+  const elements = document.querySelectorAll("h2, h3");
+
+  for (const element of elements) {
+    observer.value.observe(element);
+  }
+});
+
+onBeforeUnmount(() => {
+  if (observer.value instanceof IntersectionObserver) {
+    observer.value.disconnect();
+  }
+});
 </script>
