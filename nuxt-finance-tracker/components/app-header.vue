520417dfa5960dcf65e82781a09239830901f011
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-09 14:54:47 +0900

    035-nuxtui-layout-header

---
+++ nuxt-finance-tracker/components/app-header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header
+    class="flex justify-between items-center mt-10 mx-auto max-w-4xl w-[94%]"
+  >
+    <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </header>
+</template>
