69c52d418e613b7dc55e80a717e5e476894afbf6
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-09 16:44:16 +0900

    037-trend-icons-and-loader

--- nuxt-finance-tracker/pages/index.vue
+++ nuxt-finance-tracker/pages/index.vue
@@ -5,6 +5,39 @@
       <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
     </div>
   </section>
+
+  <section
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-8"
+  >
+    <Trend
+      color="green"
+      title="Income"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="false"
+    />
+    <Trend
+      color="red"
+      title="Expense"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="false"
+    />
+    <Trend
+      color="green"
+      title="Income"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="false"
+    />
+    <Trend
+      color="red"
+      title="Expense"
+      :amount="4000"
+      :last-amount="3000"
+      :loading="true"
+    />
+  </section>
 </template>
 
 <script setup>
