a43a9f5f81dec350bc23727ea6e0b6a165deefa7
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-16 15:12:54 +0900

    066-admin-actions-seeder

--- nuxt-finance-tracker/seed.mjs
+++ nuxt-finance-tracker/seed.mjs
@@ -4,12 +4,18 @@ import "dotenv/config";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
+  process.env.SUPABASE_SERVICE_ROLE,
   {
     auth: { persistSession: false },
   }
 );
 const categories = ["Food", "Housing", "Car", "Entertainment"];
+
+const {
+  data: { users },
+  error,
+} = await supabase.auth.admin.listUsers();
+const userIds = users.map((user) => user.id);
 
 async function seedTransactions() {
   // Delete existing data
@@ -25,54 +31,57 @@ async function seedTransactions() {
 
   let transactions = [];
 
-  // 날짜 설정 current year & last year
-  for (
-    let year = new Date().getFullYear();
-    year > new Date().getFullYear() - 2;
-    year--
-  ) {
-    for (let i = 0; i < 10; i++) {
-      const date = new Date(
-        year,
-        faker.number.int({ min: 0, max: 11 }), // month
-        faker.number.int({ min: 1, max: 28 }) // day
-      );
+  for (const user of userIds) {
+    // 날짜 설정 current year & last year
+    for (
+      let year = new Date().getFullYear();
+      year > new Date().getFullYear() - 2;
+      year--
+    ) {
+      for (let i = 0; i < 10; i++) {
+        const date = new Date(
+          year,
+          faker.number.int({ min: 0, max: 11 }), // month
+          faker.number.int({ min: 1, max: 28 }) // day
+        );
 
-      let type, category;
-      const typeBias = Math.random();
+        let type, category;
+        const typeBias = Math.random();
 
-      if (typeBias < 0.85) {
-        type = "Expense";
-        category = faker.helpers.arrayElement(categories); // Category only for 'Expense'
-      } else if (typeBias < 0.95) {
-        type = "Income";
-      } else {
-        type = faker.helpers.arrayElement(["Saving", "Investment"]);
+        if (typeBias < 0.85) {
+          type = "Expense";
+          category = faker.helpers.arrayElement(categories); // Category only for 'Expense'
+        } else if (typeBias < 0.95) {
+          type = "Income";
+        } else {
+          type = faker.helpers.arrayElement(["Saving", "Investment"]);
+        }
+
+        let amount;
+        switch (type) {
+          case "Income":
+            amount = faker.number.int({ min: 2000000, max: 5000000 });
+            break;
+          case "Expense":
+            amount = faker.number.int({ min: 100000, max: 1000000 });
+            break;
+          case "Saving":
+          case "Investment":
+            amount = faker.number.int({ min: 5000000, max: 10000000 });
+            break;
+          default:
+            amount = 0;
+        }
+
+        transactions.push({
+          created_at: date,
+          amount,
+          type,
+          description: faker.lorem.sentence(),
+          category: type === "Expense" ? category : null,
+          user_id: user,
+        });
       }
-
-      let amount;
-      switch (type) {
-        case "Income":
-          amount = faker.number.int({ min: 2000000, max: 5000000 });
-          break;
-        case "Expense":
-          amount = faker.number.int({ min: 100000, max: 1000000 });
-          break;
-        case "Saving":
-        case "Investment":
-          amount = faker.number.int({ min: 5000000, max: 10000000 });
-          break;
-        default:
-          amount = 0;
-      }
-
-      transactions.push({
-        created_at: date,
-        amount,
-        type,
-        description: faker.lorem.sentence(),
-        category: type === "Expense" ? category : null,
-      });
     }
   }
 
@@ -83,7 +92,7 @@ async function seedTransactions() {
   if (insertError) {
     console.error("Error inserting data:", insertError);
   } else {
-    console.log("Data inserted successfly");
+    console.log("Data inserted successfully");
   }
 }
 
