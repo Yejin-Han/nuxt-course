09e38ed8ed0586017b13f76ac3762371a9425436
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-16 11:10:11 +0900

    065-sign-out

--- nuxt-finance-tracker/components/app-header.vue
+++ nuxt-finance-tracker/components/app-header.vue
@@ -4,10 +4,32 @@
   >
     <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
     <div class="flex items-center space-x-4">
-      <UAvatar
-        src="https://avatars.githubusercontent.com/u/739984?v=4"
-        alt="Avatar"
-      />
+      <UDropdown
+        :items="items"
+        :ui="{ item: { disabled: 'cursor-text select-text' }, width: 'w-64' }"
+        v-if="user"
+      >
+        <UAvatar
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          alt="Avatar"
+        />
+        <template #account="{ item }">
+          <div class="text-left">
+            <p>Signed in as</p>
+            <p class="font-medium text-gray-900 dark:text-white">
+              {{ item.label }}
+            </p>
+          </div>
+        </template>
+
+        <template #item="{ item }">
+          <span class="truncate">{{ item.label }}</span>
+          <UIcon
+            :name="item.icon"
+            class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto"
+          />
+        </template>
+      </UDropdown>
       <!-- 임의로 색상모드 토글버튼 추가 -->
       <button
         class="rounded-full w-8 h-8 bg-gray-800 dark:bg-gray-200 text-2xl"
@@ -20,6 +42,37 @@
 </template>
 
 <script setup>
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+
+const items = [
+  [
+    {
+      label: user.value?.email,
+      slot: "account",
+      disabled: true,
+    },
+  ],
+  [
+    {
+      label: "Settings",
+      icon: "i-heroicons-cog-8-tooth",
+      click: () => console.log("Link to settings in the future"),
+    },
+  ],
+  [
+    {
+      label: "Sign out",
+      icon: "i-heroicons-arrow-left-on-rectangle",
+      click: async () => {
+        await supabase.auth.signOut();
+        return navigateTo("/login");
+      },
+    },
+  ],
+];
+
+//////////////////////////////////////////////////////////////////////////////////
 const isDarkMode = ref(true);
 const currentModeIcon = computed(() => (isDarkMode.value ? "🌑" : "🌕"));
 const toggleMode = () => {
