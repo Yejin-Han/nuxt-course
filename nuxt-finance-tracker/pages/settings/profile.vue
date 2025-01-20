3f73b0f2d0241304b0c116c8e364335ff2e5f3fe
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-20 11:05:35 +0900

    071-user-profile-saving

--- nuxt-finance-tracker/pages/settings/profile.vue
+++ nuxt-finance-tracker/pages/settings/profile.vue
@@ -1,5 +1,6 @@
 <template>
-  <UForm :state="state" :schema="schema">
+  <UForm :state="state" :schema="schema" @submit="saveProfile">
+    <!-- *궁금했던 점: 왜 @submit.prevent가 아니고 @submit인가 => UForm에서는 기본적으로 e.preventDefault()가 포함되어 있어서 자동으로 새로고침을 막아줌! -->
     <UFormGroup class="mb-4" label="Full Name" name="name">
       <UInput v-model="state.name" />
     </UFormGroup>
@@ -16,7 +17,8 @@
       color="black"
       variant="solid"
       label="Save"
-      :pending="pending"
+      :loading="pending"
+      :disabled="pending"
     />
   </UForm>
 </template>
@@ -31,7 +33,7 @@ const { toastSuccess, toastError } = useAppToast();
 const pending = ref(false);
 
 const state = ref({
-  name: "",
+  name: user.value.user_metadata?.full_name,
   email: user.value.email,
 });
 
@@ -40,8 +42,36 @@ const schema = z.object({
   email: z.string().email(),
 });
 
-/* const { data, error } = await supabase.auth.updateUser({
-  email: "",
-  data: {},
-}); */
+const saveProfile = async () => {
+  pending.value = true;
+
+  try {
+    const data = {
+      data: {
+        full_name: state.value.name,
+      },
+    };
+
+    if (state.value.email !== user.value.email) {
+      data.email = state.value.email;
+    }
+    console.log(data);
+
+    const { error } = await supabase.auth.updateUser(data);
+
+    if (error) throw error;
+
+    toastSuccess({
+      title: "Profile updated",
+      description: "Your profile has been updated.",
+    });
+  } catch (err) {
+    toastError({
+      title: "Failed updating Profile",
+      description: err.message,
+    });
+  } finally {
+    pending.value = false;
+  }
+};
 </script>
