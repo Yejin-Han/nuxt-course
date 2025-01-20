f3e978342962764b73b17827a90b9d2a3f4e1893
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-20 16:22:26 +0900

    074-upload-and-rls

--- nuxt-finance-tracker/pages/settings/avatar.vue
+++ nuxt-finance-tracker/pages/settings/avatar.vue
@@ -1,1 +1,78 @@
-<template>Avatar</template>
+<template>
+  <div>
+    <div class="mb-4">
+      <UFormGroup
+        label="Current avatar"
+        class="w-full"
+        help="This would be blank by default"
+      >
+        <UAvatar
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          size="3xl"
+        />
+      </UFormGroup>
+    </div>
+    <div class="mb-4">
+      <UFormGroup
+        label="New Avatar"
+        class="w-full"
+        namve="avatar"
+        help="After choosing an image click Save to actually upload the new avatar"
+      >
+        <UInput type="file" ref="fileInput" />
+      </UFormGroup>
+    </div>
+
+    <UButton
+      type="submit"
+      color="black"
+      variant="solid"
+      label="Save"
+      :loading="uploading"
+      :disabled="uploading"
+      @click="saveAvatar"
+    />
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+const { toastSuccess, toastError } = useAppToast();
+
+const uploading = ref(false);
+const fileInput = ref(); // Input ref="fileInput" 속성의 레퍼런스
+
+const saveAvatar = async () => {
+  // 1. Get the uploaded file
+  //    a) If no file uploaded, show toast error
+  // 2. Generate the new filename
+  const file = fileInput.value.input.files[0];
+  if (!file) {
+    toastError({ title: "Select a file to upload first" });
+    return;
+  }
+
+  console.log(file);
+
+  try {
+    uploading.value = true;
+    // 1. Grab the current avatar URL
+    // 2. Upload the image to avatars bucket
+    // 3. Update the user metadata with the avatar URL
+    // 4. (OPTIONALLY) Remove the old avatar file
+    // 5. Reset the file input
+
+    toastSuccess({
+      title: "Avatar uploaded",
+    });
+  } catch (err) {
+    toastError({
+      title: "Failed uploading avatar",
+      description: err.message,
+    });
+  } finally {
+    uploading.value = false;
+  }
+};
+</script>
