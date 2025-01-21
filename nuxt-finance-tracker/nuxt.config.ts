d9b795fbb51fc569580719747dafcb0f598f43a1
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-21 16:41:06 +0900

    080-deploying-finance-tracker

--- nuxt-finance-tracker/nuxt.config.ts
+++ nuxt-finance-tracker/nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxt/ui", "@nuxtjs/supabase"],
   supabase: {
     redirect: true,
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
+    },
   },
 });
